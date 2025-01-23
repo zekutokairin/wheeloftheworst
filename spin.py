08a137496ec7a6ccd82851df98a3c7de700366e0
Zeku Tokairin <zeku@heckfire.club>
2025-01-22 20:47:10 -0500

    Initial commit on Windows oh boy CRLF

---
+++ spin.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+
+WINPATH = "C:\\Users\\user\\apps\\SuperVHS_v1.2-win"
+
+MOVIEPATH = os.path.join("SuperVHS","Content","Movies")
+
+PATH = os.path.join(WINPATH,MOVIEPATH)
+
+print("Hello world") 
+print(PATH)
+
+
