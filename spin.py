6f5281fa4bd5051799c45e5935548d9081ffb4ac
Zekū Tōkairin <zeku@heckfire.club>
2025-01-23 00:53:25 -0500

    Checking for platform and path works

--- spin.py
+++ spin.py
@@ -3,22 +3,24 @@
 import os
 import platform
 
-# Path in the SuperVHS directory
 WINPATH = "C:\\Users\\user\\apps\\SuperVHS_v1.2-win"
-# Path within the .app file
-MACPATH = "/Contents/UE4/SuperVHS/Content/Movies"
-
-MOVIEPATH = os.path.join("SuperVHS","Content","Movies")
+MACPATH = "/Users/user/Games/SuperVHS_v1.2-mac/SuperVHS.app"
 
 if platform.system() == "Darwin":
+    MOVIEPATH = os.path.join("Contents","UE4","SuperVHS","Content","Movies")
     PATH = os.path.join(MACPATH,MOVIEPATH)
 
 elif platform.system() == "Windows":
+    MOVIEPATH = os.path.join("SuperVHS","Content","Movies")
     PATH = os.path.join(WINPATH,MOVIEPATH)
 
 else:
     raise Exception("Unsupported OS: %s" % platform.system())
 
+if not os.path.exists(os.path.join(PATH,"backup")):
+    # TODO: Create it
+    # TODO: Move movies over
+    raise NotImplementedError
 
 import code
 #code.interact(local=locals())
@@ -39,6 +41,6 @@ MOVIES = ["RLMTV_DD1.mov",
 "RLMTV_Twisted1.mov",
 "RLMTV_Twisted2.mov"]
 
-print("Hello world") 
 print(PATH)
+print(os.listdir(PATH))
 
