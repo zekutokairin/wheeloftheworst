82f60f3507fc49ab1f6b83d5d2ddd8958bab801e
Zekū Tōkairin <zeku@heckfire.club>
2025-01-22 23:19:59 -0500

    Initial commit on Mac side, conflict incoming

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # wheeloftheworst
-Lets you effect your own WOTW within SuperVHS
+Lets you create your own WOTW within SuperVHS!
+
+Just point it at a directory full of movies, and it will pick a number,
+and insert them into SuperVHS to play on the Plinkett TV.
+
+TODO:
+- Create a backup of all the videos
+- Check to see if we can use Windows soft links or symlinks for videos
+
+Future:
+- Scan for likely locations of SuperVHS
+- Uninstall option to restore videos to stock settings
+
---
+++ spin.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Path in the SuperVHS directory
+WINPATH = "TODO"
+# Path within the .app file
+MACPATH = "/Contents/UE4/SuperVHS/Content/Movies"
+
+MOVIES = ["RLMTV_DD1.mov",
+"RLMTV_DD2.mov",
+"RLMTV_FF1.mov",
+"RLMTV_FF2.mov",
+"RLMTV_Hawaii1.mov",
+"RLMTV_Hawaii2.mov",
+"RLMTV_Ishtar.mov",
+"RLMTV_MacMe.mov",
+"RLMTV_NightCourt.mov",
+"RLMTV_SWHS1.mov",
+"RLMTV_SWHS2.mov",
+"RLMTV_Sasquatch.mov",
+"RLMTV_Shimmy.mov",
+"RLMTV_Twisted1.mov",
+"RLMTV_Twisted2.mov"]
+
+if __name__ == "__main__":
+    print("Hello world")
