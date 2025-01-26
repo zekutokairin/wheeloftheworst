1992f41aeb25aa220257345225d59a9a8c9022be
Zekū Tōkairin <zeku@heckfire.club>
2025-01-26 00:23:49 -0500

    Just need to do the actual copy and it'll be done.

--- spin.py
+++ spin.py
@@ -3,10 +3,15 @@
 import os
 import sys
 import platform
+import shutil
+import random
 
 from config import *
 
 BAKPATH = None
+
+def usage():
+    print("./spin.py <Directory with movies>")
 
 if platform.system() == "Darwin":
     MOVIEPATH = os.path.join("Contents","UE4","SuperVHS","Content","Movies")
@@ -30,15 +35,35 @@ if not os.path.exists(BAKPATH):
 # Copies up to 14 of them into the SuperVHS movie folder
 def spinTheWheel(movie_entries):
     for entry in movie_entries:
+        print("Copying in movie: %s" % entry)
         # TODO cp entry
-        pass
+        # shutil.copy(entry, target)
 
 if __name__ == "__main__":
     print(PATH)
     print(os.listdir(PATH))
 
-    args = sys.argv[1:]
-    # TODO: Clean out movie directory before 
-    wheeldir = 
+    try:
+        args = sys.argv[1:]
+        sourcemovie_dir = args[0]
+    except IndexError:
+        sys.exit(usage)
+
+    if os.path.isdir(sourcemovie_dir):
+        # It's a directory, pass in the files
+        # TODO make sure to use full paths
+        movies = os.listdir(sourcemovie_dir)
+        fullpath_movies = []
+        random.seed()
+        for movie in movies:
+            if os.path.isdir(movie):
+                continue
+            else:
+                fullpath_movies.append(os.path.join(sourcemovie_dir,movie))
+        random.shuffle(fullpath_movies)
+        spinTheWheel(movies)
+
+    # TODO: Clean out movie directory before copying new ones in
+    #wheeldir = 
     print(args)
 
